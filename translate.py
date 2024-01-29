e2232338106efcba24d62f8756017a54d1751f09
Marius Iurea <marius.iurea@yahoo.com>
2024-01-29 21:48:14 +0100

    added gitignore and sorted language list

--- translate.py
+++ translate.py
@@ -2,6 +2,6 @@ from deep_translator import GoogleTranslator
 import random
 langs_list = GoogleTranslator().get_supported_languages()
 def translate_to_greek(text):    
-    random_lang = random.sample(set(langs_list), 1)    
+    random_lang = random.sample(sorted(set(langs_list)), 1)    
     translated_text = GoogleTranslator(source='auto', target = str(random_lang[0])).translate(text)
     return translated_text
